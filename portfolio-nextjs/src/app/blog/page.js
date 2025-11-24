733d968cd7cc3490fd7b2a1638d9a180565c178d
jjaayy2222 <qkfkadmlEkf@gmail.com>
2025-11-24 18:07:37 +0900

    fix: 빈 블로그 페이지로 인한 빌드 에러 수정

---
+++ portfolio-nextjs/src/app/blog/page.js
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <div className="max-w-4xl mx-auto pt-20 pb-20 text-center">
+      <h1 className="text-4xl font-bold mb-4">Blog</h1>
+      <p className="text-gray-500">Coming soon...</p>
+    </div>
+  );
+}
